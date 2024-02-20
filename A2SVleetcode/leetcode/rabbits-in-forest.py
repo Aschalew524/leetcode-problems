617346226fc0f464512b4bd3094edec949e225db
Aschalew Abayneh <143227429+Aschalew524@users.noreply.github.com>
2024-02-20 21:55:13 +0300

    Add solution for Rabbits in Forest

--- A2SVleetcode/leetcode/rabbits-in-forest.py
+++ A2SVleetcode/leetcode/rabbits-in-forest.py
@@ -1,15 +1,8 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        d = {}
-        count = 0
-        for i in answers:
-            if i == 0:
-                count+=1
-            else:
-                if i not in d or i == d[i]:
-                    d[i] = 0
-                    count+=1 + i
-                else:
-                    d[i] += 1
-                    
-        return count
+        rabbits = 0
+        colors= Counter(answers)
+        for rabbit,color in colors.items():
+            number = math.ceil(color/(rabbit+1))
+            rabbits = rabbits + (number * (rabbit+1))
+        return rabbits
