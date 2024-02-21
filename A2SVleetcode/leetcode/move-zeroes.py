cf7bad53a7f6749f4bf443782e4bf9f376f19ce3
Aschalew Abayneh <143227429+Aschalew524@users.noreply.github.com>
2024-02-21 19:47:19 +0300

    Add solution for Move Zeroes

---
+++ A2SVleetcode/leetcode/move-zeroes.py
@@ -0,0 +1,20 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l,r=0,1
+        while r < len(nums):
+            if nums[l]!=0:
+                l+=1
+                r+=1
+            elif nums[r]==0:
+                r+=1
+            else:
+                if nums[l]==0 and nums[r]!=0:
+                    temp=nums[l]
+                    nums[l]=nums[r]
+                    nums[r]=temp
+
+
+        
