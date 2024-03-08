9ca75d2dc5cddb1f04638608f47e05da2c2fbeed
Aschalew Abayneh <143227429+Aschalew524@users.noreply.github.com>
2024-03-08 23:15:43 +0300

    Add solution for Maximum Ice Cream Bars

---
+++ A2SVleetcode/leetcode/maximum-ice-cream-bars.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        count = 0
+        for i in costs:
+            if(coins>=i):
+                count+=1
+                coins-=i
+        return count
