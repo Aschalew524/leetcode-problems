974409b0cec05d5bf5df860449a0cddf422ad17a
Aschalew Abayneh <143227429+Aschalew524@users.noreply.github.com>
2024-05-22 19:46:09 +0300

    Created 'Longest Common Prefix' by A2SV Hub

---
+++ 22-May-2024/Longest Common Prefix 157409.py
@@ -0,0 +1,15 @@
+# Problem: Longest Common Prefix - https://leetcode.com/problems/longest-common-prefix/
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs.sort()
+        ans=""
+        first=strs[0]
+        last=strs[-1]
+        for i in range(len(first)):
+            if  first[i]!=last[i] or i==len(last):
+                return ans
+            ans+=first[i]
+        return ans
+
+        
