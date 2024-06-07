19c6bd41a8bb29a9f4e07935954813ab11a520b7
Aschalew Abayneh <143227429+Aschalew524@users.noreply.github.com>
2024-06-07 22:24:43 +0300

    Created 'A - Double it and give it to the next person.' by A2SV Hub

---
+++ A2SV G5 - Contest #19 07-Jun-2024/A - Double it and give it to the next person. 165596.py
@@ -0,0 +1,23 @@
+# Problem: A - Double it and give it to the next person. - https://codeforces.com/gym/527294/problem/A
+
+t=int(input())
+for i in range(t):
+    st=""
+    ans=""
+    res=""
+    s=input()
+    for c in s:
+        st+=c
+        st+=c
+    st=sorted(st)
+    for r in range(0,len(st),2):
+        ans+=st[r]
+        if r+1 < len(st):
+            res+=st[r+1]
+    res=sorted(res,reverse=True)
+    res="".join(res)
+    print(ans+res)
+    
+    
+
+
