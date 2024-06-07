5e62dc9753c2b4982fb94c52226920d647978725
Aschalew Abayneh <143227429+Aschalew524@users.noreply.github.com>
2024-06-07 22:24:12 +0300

    Created 'B - Eba loves math.' by A2SV Hub

---
+++ A2SV G5 - Contest #19 07-Jun-2024/B - Eba loves math. 165595.py
@@ -0,0 +1,12 @@
+# Problem: B - Eba loves math. - https://codeforces.com/gym/527294/problem/B
+
+t=int(input())
+for i in range(t):
+    n=int(input())
+    lis=list(map(int,input().split()))
+    ans=lis[0]
+    for i in range(1,n):
+        ans=lis[i] & ans
+    print(ans)
+
+    
