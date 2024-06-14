2d6fa5c65b65ad891ff76989ea48964825551acd
Aschalew Abayneh <143227429+Aschalew524@users.noreply.github.com>
2024-06-14 23:29:33 +0300

    Created 'A - Spris' by A2SV Hub

---
+++ A2SV G5 - Contest #20 14-Jun-2024/A - Spris 168500.py
@@ -0,0 +1,12 @@
+# Problem: A - Spris - https://codeforces.com/gym/528792/problem/A
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+ma = a
+av = b // 2
+ban = c // 4
+
+tot= min(ma, av, ban)
+print(tot* 7)
