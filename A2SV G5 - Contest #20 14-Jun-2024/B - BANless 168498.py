a84e4cbee56326bd1afcb77224c4c86fc5377b4d
Aschalew Abayneh <143227429+Aschalew524@users.noreply.github.com>
2024-06-14 23:26:56 +0300

    Created 'B - BANless' by A2SV Hub

---
+++ A2SV G5 - Contest #20 14-Jun-2024/B - BANless 168498.py
@@ -0,0 +1,21 @@
+# Problem: B - BANless - https://codeforces.com/gym/528792/problem/B
+
+for i in range(int(input())):
+    n=int(input())
+    s="BAN"*n
+    ans=[]
+    l=0
+    r=len(s)-1
+    count=0
+    while l < r:
+        ans.append((l+1,r+1))
+        count+=1
+        r-=3
+        l+=3
+    print(count)
+    for an in ans:
+        print(*an)
+
+
+
+
