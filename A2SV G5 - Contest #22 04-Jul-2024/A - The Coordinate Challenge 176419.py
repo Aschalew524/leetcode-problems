7967e1321f2446c0d0de20c361ebf33bc755ec21
Aschalew Abayneh <143227429+Aschalew524@users.noreply.github.com>
2024-07-04 22:22:40 +0300

    Created 'A - The Coordinate Challenge' by A2SV Hub

---
+++ A2SV G5 - Contest #22 04-Jul-2024/A - The Coordinate Challenge 176419.py
@@ -0,0 +1,16 @@
+# Problem: A - The Coordinate Challenge - https://codeforces.com/gym/531455/problem/A
+
+for i in range(int(input())):
+    n=int(input())
+    rem=n % 3
+    if n==1:
+        print(2)
+
+    elif n % 3 == 0:
+        print(n//3)
+    elif n % 3 == 2:
+        print((n//3) + 1)
+    elif n % 3 == 1:
+        print((n-4)//3 + 2)
+
+       
