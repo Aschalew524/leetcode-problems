abd0c0e648514e90c61a328854cccad480fb5441
Aschalew Abayneh <143227429+Aschalew524@users.noreply.github.com>
2024-07-25 22:30:53 +0300

    Created 'A - Se7en' by A2SV Hub

---
+++ A2SV G5 - Contest #26 25-Jul-2024/A - Se7en 184774.py
@@ -0,0 +1,17 @@
+# Problem: A - Se7en - https://codeforces.com/gym/537362/problem/A
+
+t=int(input())
+for i in range(t):
+    n=int(input())
+  
+    if n% 7 == 0:
+        print(n)
+    else:
+        x= n % 10
+        n=n - x
+        while n% 7 != 0:
+            n+=1
+        print(n)
+
+
+    
