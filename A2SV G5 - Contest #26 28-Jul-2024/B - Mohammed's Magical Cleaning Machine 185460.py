8c03977502c25eaa44306b4961ec3c093f1128f1
Aschalew Abayneh <143227429+Aschalew524@users.noreply.github.com>
2024-07-28 22:25:28 +0300

    Created 'B - Mohammed's Magical Cleaning Machine' by A2SV Hub

---
+++ A2SV G5 - Contest #26 28-Jul-2024/B - Mohammed's Magical Cleaning Machine 185460.py
@@ -0,0 +1,16 @@
+# Problem: B - Mohammed's Magical Cleaning Machine - https://codeforces.com/gym/537362/problem/B
+
+t=int(input())
+for i in range(t):
+    n=int(input())
+    lis=list(map(int,input().split()))
+    count=0
+    i=0
+    while i < n and lis[i]==0:
+        i+=1
+    for j in range(i,n-1):
+        count+=lis[j]
+        if lis[j]==0:
+            count+=1
+    print(count)
+    
