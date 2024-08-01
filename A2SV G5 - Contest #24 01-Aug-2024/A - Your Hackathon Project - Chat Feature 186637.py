68c13829a2729304eb129a7b2e0d54ec04f7c569
Aschalew Abayneh <abayasche0@gmail.com>
2024-08-01 22:23:19 +0300

    Created 'A - Your Hackathon Project - Chat Feature' by A2SV Hub

---
+++ A2SV G5 - Contest #24 01-Aug-2024/A - Your Hackathon Project - Chat Feature 186637.py
@@ -0,0 +1,30 @@
+# Problem: A - Your Hackathon Project - Chat Feature - https://codeforces.com/gym/534160/problem/A
+
+t=int(input())
+for i in range(t):
+    n=int(input())
+    s=input()
+    i=n-1
+    if n%2==0:
+        while i >= n//2-1:
+            if s[i] == ")":
+                i-=1
+            else:
+                print("NO")
+                break
+        else:
+            print("YES")
+    else:
+        while i > n//2-1:
+            if s[i] == ")":
+                i-=1
+            else:
+                print("NO")
+                break
+        else:
+            print("YES")
+
+        
+        
+        
+            
