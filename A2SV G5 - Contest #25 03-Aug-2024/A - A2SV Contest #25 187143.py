62bd047aede5f56edaa6407115ad2a5a8b69d4dd
Aschalew Abayneh <abayasche0@gmail.com>
2024-08-03 22:50:26 +0300

    Created 'A - A2SV Contest #25' by A2SV Hub

---
+++ A2SV G5 - Contest #25 03-Aug-2024/A - A2SV Contest #25 187143.py
@@ -0,0 +1,11 @@
+# Problem: A - A2SV Contest #25 - https://codeforces.com/gym/535749/problem/A
+
+a,b,c,d=map(int,input().split())
+m=max((3*a)/10, a-a/250*c)
+v=max((3*b)/10, b-b/250*d)
+if m > v:
+    print("Misha")
+elif v > m:
+    print("Vasya")
+else:
+    print("Tie")
