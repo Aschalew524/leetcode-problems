048303b3f3dbdd542360caa37a8f0f544e76869e
Aschalew Abayneh <abayasche0@gmail.com>
2024-08-03 22:49:38 +0300

    Created 'B - Optimal Point' by A2SV Hub

---
+++ A2SV G5 - Contest #25 03-Aug-2024/B - Optimal Point 187142.py
@@ -0,0 +1,30 @@
+# Problem: B - Optimal Point - https://codeforces.com/gym/535749/problem/B
+
+from typing import Counter
+
+
+t=int(int(input()))
+for i in range(t):
+    n,k=map(int,input().split())
+    cnt=Counter()
+    for i in range(n):
+        a,b=map(int,input().split())
+        if a <= k <= b:
+            for i in range(a,b+1):
+                cnt[i]+=1
+    lis=list(cnt.values())
+    x=cnt[k]
+    if len(cnt)== 0:
+        print("NO")
+    elif lis.count(x) > 1:
+        print("NO")
+    else:
+        if len(cnt) > 0  and cnt[k] == max(cnt.values()):
+            print("YES")
+            
+        else:
+            print("NO")
+   
+        
+    
+    
