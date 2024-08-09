a08731cee58fd1db397585ea31c6f971c5fb9e1f
Aschalew Abayneh <abayasche0@gmail.com>
2024-08-09 21:28:11 +0300

    Created 'B - Fasika and Children' by A2SV Hub

---
+++ A2SV G5 - Contest #27 09-Aug-2024/B - Fasika and Children 190080.py
@@ -0,0 +1,16 @@
+# Problem: B - Fasika and Children - https://codeforces.com/gym/538762/problem/B
+
+import math
+
+
+n,m=map(int,input().split())
+lis = list(map(int,input().split()))
+check=[]
+for i in range(n):
+    check.append(math.ceil(lis[i]/m))
+for i in range(n-1,-1,-1):
+    if check[i] == max(check):
+        print(i+1)
+        break
+else:
+    print(n)
