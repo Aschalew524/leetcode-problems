a1ed3a618926cb3dad867fbe004c83a7b7b88ad7
Aschalew Abayneh <abayasche0@gmail.com>
2024-08-09 21:28:50 +0300

    Created 'A - Split the Set' by A2SV Hub

---
+++ A2SV G5 - Contest #27 09-Aug-2024/A - Split the Set 190082.py
@@ -0,0 +1,17 @@
+# Problem: A - Split the Set - https://codeforces.com/gym/538762/problem/A
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    lis = list(map(int, input().split()))
+    odd = 0
+    even = 0
+    for i in range(len(lis)):
+        if lis[i] % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+    if odd == even == n:
+        print("YES")
+    else:
+        print("NO")
